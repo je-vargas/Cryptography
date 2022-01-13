c031df64509d0de61dabb75d453397eba5379a69
juanE <55924277+je-vargas@users.noreply.github.com>
2022-01-13 09:22:29 +0000

    definition added to credit card

--- CreditCard/credit_card_checker.py
+++ CreditCard/credit_card_checker.py
@@ -3,7 +3,7 @@
 #* 15 digits of credit card are random generated
 def read_number(readNumber):
     '''
-        DEFINITION: Read's number and transfroms it into a list
+        DEFINITION: Read's number from keyboard input and returns it into a list
     '''
     numberList = list()
     sliceUpto = 1
@@ -13,12 +13,21 @@ def read_number(readNumber):
 
 
 def doubled_digit(digit):
+    '''
+        DEFINITION: "Doubles integer passed in and checks if its bigger than 10
+                    subtracts 9 if it is. Value is returned" 
+    '''
     doubleDigit = digit * 2 
     if ( doubleDigit >= 10):
         return (doubleDigit - 9)
     return doubleDigit
 
 def credit_card_checker(cardNumber):
+    '''
+    Definition: loops through list of numbers passed, jumping by 2.
+                then adds all numbers in the list and mods by 10. 
+                if value is 0 is valid else invalid
+    '''
     cardNumbersSplit = cardNumber.copy()
     for digit in range(0, len(cardNumbersSplit) , 2):
         cardNumbersSplit[digit] = doubled_digit(cardNumbersSplit[digit])
